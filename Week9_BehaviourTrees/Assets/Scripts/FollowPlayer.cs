4f5fca42d173d097e5e512dc3b858c7d204d150f
Rogan <148461496+roganm30@users.noreply.github.com>
2025-03-20 20:49:15 -0400

    Week9Exercise_Complete
    Did the whole thing :D

---
+++ Week9_BehaviourTrees/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // I read in a forum that camera movement fits better in LateUpdate, rather than Update
+    void LateUpdate()
+    {
+        if(player != null)
+        {
+            transform.position = player.transform.position + new Vector3(0, 10, -14);
+        }
+    }
+}
---
+++ Week9_BehaviourTrees/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 7.5f;
+    private float gravityValue = -9.81f;
+
+    // credit to the official unity documentation for a simple to implement / use movement script
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
